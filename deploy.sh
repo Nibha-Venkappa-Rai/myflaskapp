78a67b97276d7ef47942cacc45959a6508c1ab79
Nibha_V_Rai <nibhavrai@gmail.com>
2025-07-05 17:10:07 +0530

    v8

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+
 sudo docker pull nibhav/pet_store_app_01:latest
 sudo docker stop pet_store_app_01 || true
 sudo docker rm pet_store_app_01 || true
